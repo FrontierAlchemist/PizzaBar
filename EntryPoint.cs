9afc4bf4de83df16e7531b73b0d2c59217c73f2b
Anton Podchuvalov <aa.podchuvalov@gmail.com>
2023-03-23 21:02:50 +0400

    Implement main functional

---
+++ EntryPoint.cs
@@ -0,0 +1,27 @@
+﻿namespace PizzaBar
+{
+	internal class EntryPoint
+	{
+		private static void Main(string[] args)
+		{
+			if (args.Length == 0) {
+				Console.WriteLine("PizzaBar need path to target folder");
+				return;
+			}
+
+			string pathToSource = "D:\\Books";
+			if (!Directory.Exists(pathToSource)) {
+				Console.WriteLine("Source folder doesn't exist");
+				return;
+			}
+
+			string pathToTarget = args[0];
+			if (!Directory.Exists(pathToTarget)) {
+				Console.WriteLine("Target folder doesn't exist");
+				return;
+			}
+
+			new Provider().Provide(pathToSource, pathToTarget);
+		}
+	}
+}
